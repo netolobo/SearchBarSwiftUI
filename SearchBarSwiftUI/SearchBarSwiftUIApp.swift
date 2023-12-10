e8a9af0aa5bac8200b39fef2e6bdd0607ffaa073
netolobo <desilio@gmail.com>
2023-12-10 14:13:18 -0800

    Initial Commit

---
+++ SearchBarSwiftUI/SearchBarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarSwiftUIApp.swift
+//  SearchBarSwiftUI
+//
+//  Created by Neto Lobo on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
